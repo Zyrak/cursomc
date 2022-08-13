2888cd701dbcd9549142631af072aaa47dc3708f
Zyrak <zyrak.br@gmail.com>
2022-08-13 17:21:52 -0300

    Projeto criado

---
+++ src/test/java/com/zyrak/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyrak.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
